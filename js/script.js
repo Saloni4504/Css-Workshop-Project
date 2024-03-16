72a117a8e2ce3c1c1335f50cff45be1e2ef82af2
Saloni Bhagat <salonibhagatt@gmail.com>
2024-03-16 23:19:52 +0530

    css-project

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.getElementById("world-link").addEventListener("mouseover", function() {
+    this.style.color = "red";
+});
+
+document.getElementById("world-link").addEventListener("mouseout", function() {
+    this.style.color = "#fff";
+});
+
+
